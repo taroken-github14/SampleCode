aafab9b600cc717f840056a6bb536301ae868bf3
taroken-github14 <taroken.ue103317@gmail.com>
2017-03-30 15:21:43 +0900

    Add　Navigation Sample

---
+++ NavigationSample/NavigationSample/NavigationSample/HomePage.xaml.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace NavigationSample
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        // ログインボタンの押下
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+
+            if (string.Format(UserId.Text).Equals("nagai") &&
+                string.Format(Password.Text).Equals("pass"))
+            {
+                await Navigation.PushAsync(new Page1(UserId.Text));
+                ErrorMessageLabel.Text = "";
+                return;
+            }
+
+            ErrorMessageLabel.Text = "ログインIDまたはパスワードが誤っています";
+
+        }
+
+  
+    }
+}
---
+++ NavigationSample/NavigationSample/NavigationSample/Page1.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace NavigationSample
+{
+    public partial class Page1 : ContentPage
+    {
+        // 遷移元 MainEntry.Text = parameter に名前データを受け渡す
+        public Page1(string parameter)
+        {
+            InitializeComponent();
+
+            // 遷移元 MainEntry.Text 名前データを表示
+            MainLabel.Text = "こんにちは" +　parameter +"さん";
+        }
+    }
+}
